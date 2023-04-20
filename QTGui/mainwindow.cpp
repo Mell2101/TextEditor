283891bc7b3773e851b0c56fb4b3a08084a2ac99
Flynn <seyfullin.contact@icloud.com>
2023-04-20 12:34:37 +0300

    Fixed Qt5 incompatibility

--- QTGui/mainwindow.cpp
+++ QTGui/mainwindow.cpp
@@ -36,13 +36,12 @@ MainWindow::MainWindow(QWidget *parent)
     //connect(fileMenu->addAction(tr("Exit")), &QAction::triggered, this, &MainWindow::ExitProgramm);
 
     QMenu* editMenu = menuBar()->addMenu(tr("Edit"));
-
-    connect(editMenu->addAction(tr("Undo"), QKeySequence("Ctrl+Z")), &QAction::triggered, m_pTextArea, &CustomTextEdit::undo);
-    connect(editMenu->addAction(tr("Redo"), QKeySequence(tr("Ctrl+Shift+Z"))), &QAction::triggered, m_pTextArea, &CustomTextEdit::redo);
-    connect(editMenu->addAction(tr("Cut"), QKeySequence(tr("Ctrl+X"))), &QAction::triggered, m_pTextArea, &CustomTextEdit::cut);
-    connect(editMenu->addAction(tr("Copy"), QKeySequence(tr("Ctrl+C"))), &QAction::triggered, m_pTextArea, &CustomTextEdit::copy);
-    connect(editMenu->addAction(tr("Paste"), QKeySequence(tr("Ctrl+V"))), &QAction::triggered, m_pTextArea, &CustomTextEdit::paste);
-    connect(editMenu->addAction(tr("Fonts..."), QKeySequence(tr("Ctrl+Shift+F"))), &QAction::triggered, this, &MainWindow::OpenFontDialog);
+    editMenu->addAction(tr("Undo"), m_pTextArea, &CustomTextEdit::undo, QKeySequence(tr("Ctrl+Z")));
+    editMenu->addAction(tr("Redo"), m_pTextArea, &CustomTextEdit::redo, QKeySequence(tr("Ctrl+Shift+Z")));
+    editMenu->addAction(tr("Cut"), m_pTextArea, &CustomTextEdit::cut, QKeySequence(tr("Ctrl+X")));
+    editMenu->addAction(tr("Copy"), m_pTextArea, &CustomTextEdit::copy, QKeySequence(tr("Ctrl+C")));
+    editMenu->addAction(tr("Paste"), m_pTextArea, &CustomTextEdit::paste, QKeySequence(tr("Ctrl+V")));
+    editMenu->addAction(tr("Fonts..."), this, &MainWindow::OpenFontDialog, QKeySequence(tr("Ctrl+Shift+F")));
 
     statusBar()->addWidget(m_pTextCursorPosInfo = new QLabel(this));
     connect(m_pTextArea, &CustomTextEdit::cursorPositionChanged, this, &MainWindow::updateTextCursorPosInfo);
