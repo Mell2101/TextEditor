ce749901586125ca6256e5ea7e7565411ff9dc82
lexus_jet <lexusx6@gmail.com>
2023-04-21 17:39:32 +0300

    added dummy methods in TextEditorCore such as: 	void setTextData(const std::string& content)
    	void clean()
    
    	std::vector<std::pair<size_t, size_t>> find(const std::string& token)
    
    	std::string getSegment(const std::pair<size_t, size_t>& segment) const
    
    	void eraseSegment(const std::pair<size_t, size_t>& segment)
    
    	void insertSegment(const std::string& segmentData, const size_t pos)

--- TextEditorCore/TextManager.cpp
+++ TextEditorCore/TextManager.cpp
@@ -12,4 +12,37 @@ TextManager::TextManager(const std::string& data) : data(data)
     
 }
 
+void TextManager::setTextData(const std::string &content)
+{
+    return;
+}
+
+void TextManager::clean()
+{
+    return;
+}
+
+std::vector<std::pair<size_t, size_t> > TextManager::find(const std::string &token)
+{
+    return std::vector<std::pair<size_t, size_t> >();
+}
+
+std::string TextManager::getSegment(const std::pair<size_t, size_t> &segment) const
+{
+    return std::string();
+}
+
+void TextManager::eraseSegment(const std::pair<size_t, size_t> &segment)
+{
+    return;
+}
+
+void TextManager::insertSegment(const std::string &segmentData, const size_t pos)
+{
+    return;
+}
+
+
+
+
 }// namespace TextEditorCore
