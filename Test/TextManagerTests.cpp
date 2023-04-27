af552249dff073a9cad41a71dc0b633022ff6513
lexus <lexusx6@gmail.com>
2023-04-27 22:16:50 +0300

    add tests for methods TextManager::clean() and TextManager::setTextData()

--- Test/TextManagerTests.cpp
+++ Test/TextManagerTests.cpp
@@ -1,19 +1,50 @@
 #include <TextManager.h>
 #include <catch2/catch_all.hpp>
 
-
 TEST_CASE("TextManager::TextManager()", "[TextManager::TextManager()]")
 {
     TextEditorCore::TextManager textManager;
-    const std::string& text = textManager.getTextData();
+    const std::string &text = textManager.getTextData();
     REQUIRE(text.empty());
 }
-
 
 TEST_CASE("TextManager::TextManager(str)", "[TextManager::TextManager(str)]")
 {
     TextEditorCore::TextManager textManager("blah blah!");
-    const std::string& text = textManager.getTextData();
+    const std::string &text = textManager.getTextData();
     REQUIRE(text == "blah blah!");
 }
 
+TEST_CASE("TextManager::clean()", "[TextManager::clean()]")
+{
+    {
+        TextEditorCore::TextManager textManager("blah blah!");
+        textManager.clean();
+        const std::string &text = textManager.getTextData();
+        REQUIRE(text.empty());
+    }
+
+    {
+        TextEditorCore::TextManager textManager;
+        textManager.clean();
+        const std::string &text = textManager.getTextData();
+        REQUIRE(text.empty());
+    }
+}
+
+TEST_CASE("TextManager::setTextData()", "[TextManager::setTextData()]")
+{
+    {
+        TextEditorCore::TextManager textManager("blah blah!");
+        textManager.setTextData("new text!");
+        const std::string &text = textManager.getTextData();
+        REQUIRE(text == "new text");
+    }
+
+    {
+        TextEditorCore::TextManager textManager;
+        textManager.setTextData("new text!");
+        const std::string &text = textManager.getTextData();
+        REQUIRE(text == "new text!");
+    }
+}
