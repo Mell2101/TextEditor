2dfbdae263ebe23ddcdb9a72a58958563fa3337d
lexus <lexusx6@gmail.com>
2023-04-28 17:05:34 +0300

    a

--- Test/TextManagerTests.cpp
+++ Test/TextManagerTests.cpp
@@ -4,14 +4,14 @@
 TEST_CASE("TextManager::TextManager()", "[TextManager::TextManager()]")
 {
     TextEditorCore::TextManager textManager;
-    const std::string &text = textManager.getTextData();
+    const std::string& text = textManager.getTextData();
     REQUIRE(text.empty());
 }
 
 TEST_CASE("TextManager::TextManager(str)", "[TextManager::TextManager(str)]")
 {
     TextEditorCore::TextManager textManager("blah blah!");
-    const std::string &text = textManager.getTextData();
+    const std::string& text = textManager.getTextData();
     REQUIRE(text == "blah blah!");
 }
 
@@ -20,14 +20,14 @@ TEST_CASE("TextManager::clean()", "[TextManager::clean()]")
     {
         TextEditorCore::TextManager textManager("blah blah!");
         textManager.clean();
-        const std::string &text = textManager.getTextData();
+        const std::string& text = textManager.getTextData();
         REQUIRE(text.empty());
     }
 
     {
         TextEditorCore::TextManager textManager;
         textManager.clean();
-        const std::string &text = textManager.getTextData();
+        const std::string& text = textManager.getTextData();
         REQUIRE(text.empty());
     }
 }
@@ -37,14 +37,21 @@ TEST_CASE("TextManager::setTextData()", "[TextManager::setTextData()]")
     {
         TextEditorCore::TextManager textManager("blah blah!");
         textManager.setTextData("new text!");
-        const std::string &text = textManager.getTextData();
+        const std::string& text = textManager.getTextData();
         REQUIRE(text == "new text");
     }
 
     {
         TextEditorCore::TextManager textManager;
         textManager.setTextData("new text!");
-        const std::string &text = textManager.getTextData();
+        const std::string& text = textManager.getTextData();
         REQUIRE(text == "new text!");
+    }
+
+    {
+        TextEditorCore::TextManager textManager;
+        textManager.setTextData(std::string("aboba"));
+        const std::string& text = textManager.getTextData();
+        REQUIRE(text == "aboba");
     }
 }
