4e230b84df0d841c9d3019ce80d824fe7d508857
lexus <lexusx6@gmail.com>
2023-05-05 14:53:57 +0300

    add tests for insertSegment and eraseSegment

--- Test/TextManagerTests.cpp
+++ Test/TextManagerTests.cpp
@@ -69,3 +69,89 @@ TEST_CASE("TextManager::setTextData()", "[TextManager::setTextData()]")
         REQUIRE(text.empty());
     }
 }
+
+TEST_CASE("TextManager::insertSegment()", "[TextManager::insertSegment()]")
+{
+    
+    {
+        TextEditorCore::TextManager textManager("blah blah");
+        bool result = textManager.insertSegment("new ", 0);
+        const std::string& text = textManager.getTextData();
+        REQUIRE((text == "new blah blah" && result == true));
+    }
+
+    {
+        TextEditorCore::TextManager textManager("blah blah");
+        bool result = textManager.insertSegment(std::string(), 0);
+        const std::string& text = textManager.getTextData();
+        REQUIRE((text == "blah blah" && result == true));
+    }
+
+    {
+        TextEditorCore::TextManager textManager;
+        bool result = textManager.insertSegment("new ", 0);
+        const std::string& text = textManager.getTextData();
+        REQUIRE((text == "new " && result == true));
+    }
+
+    {
+        TextEditorCore::TextManager textManager("blah blah");
+        bool result = textManager.insertSegment("new ", 125);
+        const std::string& text = textManager.getTextData();
+        REQUIRE((text == "blah blah" && result == false));
+    }
+
+    {
+        TextEditorCore::TextManager textManager;
+        bool result = textManager.insertSegment(std::string(), 0);
+        const std::string& text = textManager.getTextData();
+        REQUIRE((text == "" && result == true));
+    }
+}
+
+TEST_CASE("TextManager::eraseSegment()", "[TextManager::eraseSegment()]")
+{
+    
+    {
+        TextEditorCore::TextManager textManager("blah blah");
+        bool result = textManager.eraseSegment(std::make_pair(0,5));
+        const std::string& text = textManager.getTextData();
+        REQUIRE((text == " blah" && result == true));
+    }
+
+    // tests erasing segment of 0 length
+    {
+        TextEditorCore::TextManager textManager("blah blah");
+        bool result = textManager.eraseSegment(std::make_pair(0,0));
+        const std::string& text = textManager.getTextData();
+        REQUIRE((text == "blah blah" && result == true));
+    }
+    // test erasing of empty TextManager
+    {
+        TextEditorCore::TextManager textManager;
+        bool result = textManager.eraseSegment(std::make_pair(0,5));
+        const std::string& text = textManager.getTextData();
+        REQUIRE((text == "" && result == false));
+    }
+    // test incorect eraseSegment range
+    {
+        TextEditorCore::TextManager textManager("blah blah");
+        bool result = textManager.eraseSegment(std::make_pair(5,0));
+        const std::string& text = textManager.getTextData();
+        REQUIRE((text == "blah blah" && result == false));
+    }
+    // test erasing rage greater then text
+    {
+        TextEditorCore::TextManager textManager("blah blah");
+        bool result = textManager.eraseSegment(std::make_pair(1,123));
+        const std::string& text = textManager.getTextData();
+        REQUIRE((text == "b" && result == true));
+    }
+    // erasing 0 length segment from emty TextManager
+    {
+        TextEditorCore::TextManager textManager;
+        bool result = textManager.eraseSegment(std::make_pair(0,0));
+        const std::string& text = textManager.getTextData();
+        REQUIRE((text == "" && result == true));
+    }
+}
