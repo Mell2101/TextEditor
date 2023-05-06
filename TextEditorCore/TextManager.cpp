e9fdd2fc604bf3797b338822840e16ad5d6e6cb6
Nikita <nikitastrel4@gmail.com>
2023-05-06 18:05:19 +0400

    TextManager methods implemented

--- TextEditorCore/TextManager.cpp
+++ TextEditorCore/TextManager.cpp
@@ -14,11 +14,13 @@ TextManager::TextManager(const std::string& data) : data(data)
 
 void TextManager::setTextData(const std::string &content)
 {
+    data = content;
     return;
 }
 
 void TextManager::clean()
 {
+    data = "";
     return;
 }
 
@@ -34,11 +36,30 @@ std::string TextManager::getSegment(const std::pair<size_t, size_t> &segment) co
 
 void TextManager::eraseSegment(const std::pair<size_t, size_t> &segment)
 {
+    size_t it = segment.first;
+
+    while (it != segment.second)
+    {     
+        data.erase(it);
+        it++;
+    }
+    
     return;
 }
 
 void TextManager::insertSegment(const std::string &segmentData, const size_t pos)
 {
+    if (data.back() == pos)
+    {
+        data.append(segmentData, 0, segmentData.back());
+    }
+    else if (data.back() != pos)
+    {
+        data.insert(pos,segmentData);
+    }
+    
+    
+    
     return;
 }
 
