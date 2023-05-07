064fb72edb7777a6382b55b924f6919fc1e9fd74
Nikita <nikitastrel4@gmail.com>
2023-05-08 00:53:07 +0400

    Fixed problems delet exeption realisation, aded if/else realisation

--- TextEditorCore/TextManager.cpp
+++ TextEditorCore/TextManager.cpp
@@ -24,56 +24,67 @@ void TextManager::clean()
 
 std::vector<std::pair<size_t, size_t> > TextManager::find(const std::string &token)
 {
-    return std::vector<std::pair<size_t, size_t>> (1, std::pair<size_t, size_t>(0,0));
+    std::vector<std::pair<size_t, size_t> > Result;
+    
+    while (!std::string::npos)
+    {
+        size_t pos = 0;
+        
+
+        Result.push_back(std::pair<size_t, size_t>(data.find(token,pos), token.length()));
+        pos += token.length() + 1;
+    }
+    
+    return Result;
 }
 
 std::string TextManager::getSegment(const std::pair<size_t, size_t> &segment) const
 {
-    return std::string("0");
+    if (data.length() < segment.first)
+    {
+        return std::string("");
+    }
+    else
+    {
+        return data.substr(segment.first, segment.second);
+    }
 }
 
 bool TextManager::eraseSegment(const std::pair<size_t, size_t> &segment)
 {
-    try
-    {
-        data.erase(segment.first, segment.second);
-    }
-    catch(const std::exception& e)
+    if (data.length() < segment.first)
     {
         return false;
     }
-    
+    else
+    {
+        data.erase(segment.first, segment.second);
         return true;
+    }
 }
 
 bool TextManager::insertSegment(const std::string &segmentData, const size_t pos)
 {
-    try
-    {
-        if (data.length() < pos)
-        {
-            size_t tempPosition = pos - data.length();
-
-            for (size_t i = tempPosition; i < pos; i++)
-            {
-                data.insert(tempPosition, " ");
-            }
-            
-            data.insert(pos,segmentData);
-        }
-        else if (data.length() >= pos)
-        {
-            data.insert(pos, segmentData);
-        }
-    }
-    catch(const std::exception& e)
+    if (data.length() < pos)
     {
         return false;
     }
+    else if(data.back() == pos)
+    {
+        data.append(segmentData);
+        return true;
+    }
+    else if(data.front() == pos)
+    {
+        data.append(segmentData);
+        return true;
+    }
+    else
+    {
+        data.insert(pos,segmentData);
+        return true;
+    }
     
-    
-    
-    return true;
 }
 
 
