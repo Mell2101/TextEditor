ee5fcb0ad4feacc5babc97dbba3d2fd358229687
lexusjet <lexusx6@gmail.com>
2023-05-18 10:22:09 +0300

    add abstract class FileIOListener and methods pause and resume in FileManager

--- TextEditorCore/FileManager.h
+++ TextEditorCore/FileManager.h
@@ -5,8 +5,12 @@
 
 /* -------------------------------------------------------------------------- */
 
+
+
 namespace TextEditorCore
 {
+class FileIOListener;
+
 
 class FileManager //Signleton
 {
@@ -21,48 +25,74 @@ public:
         FileReadError = 5
     };
 
-    //called when load is started, argument: fileName
-    using StartLoadCallback = std::function<void (const std::string&)>;
-    //called when load is finished, arguments: fileName, dataBuffer
-    using FinishLoadCallback = std::function<void (const std::string&, std::string&)>;
-    //called when load has failed, arguments: fileName, errorCode
-    using ErrorLoadCallback = std::function<void (const std::string&, FileIOErrorsEnum)>;
-
-    //called when save is started, argument: fileName
-    using StartSaveCallback = std::function<void (const std::string&)>;
-    //called when save is finished, argument: fileName
-    using FinishSaveCallback = std::function<void (const std::string&)>;
-    //called when save has failed, arguments: fileName, errorCode
-    using ErrorSaveCallback = std::function<void (const std::string&, FileIOErrorsEnum)>;
-
+    using pauseFlag = bool;
 private:
     struct PImpl;
     std::unique_ptr<PImpl> pimpl;
     
 public:
-
-    FileManager();
+    // chunkSize - file part size we can read/write in singe iteration (Default 4 kB)
+    FileManager(const size_t chunkSize = 4096);
+    
     ~FileManager();
 
     void loadFile(
         const std::string& filePath,
         std::string& dataBuffer,
-        const StartLoadCallback& onStartLoadCallback,
-        const FinishLoadCallback& onFinishLoadCallback,
-        const ErrorLoadCallback& onErrorLoadCallback
+        FileIOListener& listner 
     );
-    
-    void stopWork();
-    
+
     void saveFile(
         const std::string& filePath,
         const std::string& dataBuffer,
         const bool isRewrite,
-        const StartSaveCallback& onStartSaveCallback,
-        const FinishSaveCallback& onFinishSaveCallback,
-        const ErrorLoadCallback& onErrorSaveCallback
+        FileIOListener& listener
     );
+
+    void pause(const std::string& filePath, FileIOListener&);
+
+    void resume(const std::string& filePath, FileIOListener&);
+
+    void stopWork();
     
 };
 
+
+class FileIOListener
+{
+public:
+    virtual ~FileIOListener();
+
+    virtual void onIOStart(const std::string& filename) = 0;
+    virtual void onIOError(const std::string& failedArguments, FileManager::FileIOErrorsEnum) = 0;
+    virtual void onProgress(const size_t percent) = 0;
+    virtual void onPause() = 0;
+    virtual void onResume() = 0;
+    virtual void onStop() = 0;
+    virtual void onLoadComplete(const std::string& fileName, std::string& dataBuffer) = 0;
+    virtual void onSaveComplete(const std::string& fileName) = 0;
+};
+
 }// namespace TextEditorCore
+
+
+
+    // //called when load is started, argument: fileName
+    // using StartLoadCallback = std::function<void (const std::string&)>;
+    // //called when load is finished, arguments: fileName, dataBuffer
+    // using FinishLoadCallback = std::function<void (const std::string&, std::string&)>;
+    // //called when load has failed, arguments: fileName, errorCode
+    // using ErrorLoadCallback = std::function<void (const std::string&, FileIOErrorsEnum)>;
+
+    // //called when save is started, argument: fileName
+    // using StartSaveCallback = std::function<void (const std::string&)>;
+    // //called when save is finished, argument: fileName
+    // using FinishSaveCallback = std::function<void (const std::string&)>;
+    // //called when save has failed, arguments: fileName, errorCode
+    // using ErrorSaveCallback = std::function<void (const std::string&, FileIOErrorsEnum)>;
+
+
+    // using BytesReadCallback = std::function<void (const size_t)>;
+    // using FilePartLoadedCallback = std::function<void (const float)>;
+
+    // using BytesSavedCallback = std::function<void (const size_t)>;
