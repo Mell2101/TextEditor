1ee3e8412a8fb985d3c1d71c9f6185f28b79f1ff
lexusjet <lexusx6@gmail.com>
2023-05-18 11:38:14 +0300

    class FileIOListener removed from FileManager.h file. add FileIOListener.h file

---
+++ TextEditorCore/FileIOListener.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+
+class FileIOListener
+{
+public:
+    enum FileIOErrorsEnum
+    {
+        NoError = 0,
+        FileDNExist = 1,
+        FileUnavailable = 2,
+        FileWriteError = 3,
+        FileReWriteTaboo = 4,
+        FileReadError = 5
+    };
+
+public:
+    virtual ~FileIOListener();
+
+    virtual void onIOStart(const std::string& filename) = 0;
+    virtual void onIOError(const std::string& failedArguments, FileIOErrorsEnum) = 0;
+    virtual void onProgress(const size_t percent) = 0;
+    virtual void onPause() = 0;
+    virtual void onResume() = 0;
+    virtual void onStop() = 0;
+    virtual void onLoadComplete(const std::string& fileName, std::string& dataBuffer) = 0;
+    virtual void onSaveComplete(const std::string& fileName) = 0;
+};
