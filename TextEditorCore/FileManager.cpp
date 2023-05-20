6e858e56fce976dfc24013fd05eddc7743b52d1e
lexusjet <lexusx6@gmail.com>
2023-05-21 01:09:45 +0300

    fixed FileManager methods

--- TextEditorCore/FileManager.cpp
+++ TextEditorCore/FileManager.cpp
@@ -21,7 +21,6 @@ namespace TextEditorCore
 
 struct FileManager::PImpl
 {
-    // using pauseFlag = bool;
 private:
     std::atomic<bool> m_pausFlag;
     std::atomic<bool> m_stopWorkFlag;
@@ -110,13 +109,17 @@ void FileManager::PImpl::loadFileFunction(const std::string& filePath,
 
             float percentageRead = 0;
             while(!file.eof())
-            {
-                std::unique_lock lock(m_fileManagerMutex);
-                m_pauseCondition.wait(lock, [a = &m_stopWorkFlag , b = &m_pausFlag ](){return (a || b);});
-                lock.unlock();
+            {                
                 if(m_stopWorkFlag)
                     break;
-
+                if(m_pausFlag)
+                {
+                    listner.onPause();
+                    std::unique_lock lock(m_fileManagerMutex);
+                    m_pauseCondition.wait(lock, [a = &m_stopWorkFlag , b = &m_pausFlag ](){return (a || !b);});
+                    if(!m_stopWorkFlag) 
+                        listner.onResume();
+                }
                 file.read(dataBuffer.data() + file.tellg(), m_chunkSize);
                 if(file.fail() && !file.eof())
                 {
@@ -151,21 +154,23 @@ void FileManager::PImpl::loadFileFunction(const std::string& filePath,
 
 void FileManager::PImpl::pause(const std::string& filePath, FileIOListener& listener)
 {
-    std::lock_guard lock(m_fileManagerMutex);
     if(m_pausFlag == true || m_stopWorkFlag)
     {
         listener.onIOError("", FileIOListener::FileWriteError);
         return;
     }
     m_pausFlag = true;
-    listener.onPause();
 }
 
 void FileManager::PImpl::resume(const std::string& filePath, FileIOListener& listener)
 {
+    if(!m_pausFlag && m_stopWorkFlag)
+    {
+        listener.onIOError("", FileIOListener::FileUnavailable);
+        return;
+    }
     m_pausFlag = false;
     m_pauseCondition.notify_one();
-    listener.onResume();
 }
 
 void FileManager::PImpl::stopWork(const std::string& filePath, FileIOListener&listener)
@@ -199,9 +204,6 @@ void FileManager::PImpl::saveFileFunction(const std::string& filePath,
             // Saving file here
             std::ofstream file(filePath);
         
-            file.close();
-            file.open(filePath);
-
             if (!file.is_open())
             {
                 if (std::filesystem::exists(filePath))
@@ -225,16 +227,21 @@ void FileManager::PImpl::saveFileFunction(const std::string& filePath,
 
             for(size_t i = 0; i < dataBuffer.size(); i += m_chunkSize)
             {
-                std::unique_lock lock(m_fileManagerMutex);
-                m_pauseCondition.wait(lock, [a = &m_stopWorkFlag , b = &m_pausFlag ](){return (a || b);});
-                lock.unlock();
                 if(m_stopWorkFlag)
                     break;
+                if(m_pausFlag)
+                {
+                    listener.onPause();
+                    std::unique_lock lock(m_fileManagerMutex);
+                    m_pauseCondition.wait(lock, [a = &m_stopWorkFlag , b = &m_pausFlag ](){return (a || !b);});
+                    if(!m_stopWorkFlag)
+                        listener.onResume();
+                }
                 file.write(dataBuffer.data() + i, m_chunkSize);
                 if (file.fail())
                 {
                     listener.onIOError(filePath, FileIOListener::FileWriteError);
-                        return;
+                    return;
                 }
                 percentageRead += percentage;
                 listener.onProgress(percentageRead);
@@ -245,7 +252,6 @@ void FileManager::PImpl::saveFileFunction(const std::string& filePath,
                 listener.onStop();
                 return;
             }
-            
             listener.onSaveComplete(filePath);
 }
 
